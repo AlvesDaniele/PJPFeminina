5ef0e84c13c61b18e4f4e8fec0fd7e78ad48102c
Daniele Ferreira <daniele62210546@alunos.sc.senac.br>
2026-03-16 21:44:56 -0300

    Minha mensagem de commit

---
+++ lista.py
@@ -0,0 +1,9 @@
+lista_compras = []
+print(lista_compras)
+
+lista_compras.append("sabão")
+lista_compras.append("amaciante")
+print(lista_compras)
+
+lista_compras.remove("sabão")
+print(lista_compras)
